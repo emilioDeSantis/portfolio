dc8f1da1ae306e5e17956269d5516df57d69e660
emilio <emilio5139@gmail.com>
2024-01-26 13:59:03 -0500

    first

---
+++ src/app/Components/Project.jsx
@@ -0,0 +1,175 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Project = ({ title, images, text, contributions, year, href }) => {
+    return (
+        <div className="project-container" style={{}}>
+            <div
+                className="project-image"
+                style={{
+                    aspectRatio: "1.7666",
+                    position: "relative",
+                    outline: "2px solid black",
+                    borderRadius: "10px",
+                    boxShadow: "0 2px 16px 0 rgba(0,0,0,0.5)",
+                }}
+            >
+                <Image
+                    fill
+                    src={images[0]}
+                    alt={"image"}
+                    sizes="50vw"
+                    priority
+                    style={{
+                        borderRadius: "10px",
+                        objectFit: "cover",
+                    }}
+                />
+            </div>
+            {/* <div>
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            alt={`Image for ${title}`}
+            width={456}
+            height={216}
+            layout="responsive"
+          />
+        ))}
+      </div> */}
+            <div
+                className="project-text"
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    // height: "31rem",
+                }}
+            >
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <h2
+                        style={{
+                            fontSize: "3.6rem",
+                            fontWeight: "600",
+                            textTransform: "uppercase",
+                            lineHeight: "1",
+                        }}
+                    >
+                        {title}
+                    </h2>
+                    <p style={{ marginTop: "2rem", opacity: 0.5 }}>{text}</p>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        fontFamily: "favorit Mono",
+                        marginTop: "4rem",
+                    }}
+                >
+                    <h3
+                        style={{
+                            fontWeight: "700",
+                            fontSize: "1.4rem",
+                            opacity: 0.6,
+                        }}
+                    >
+                        Contributions
+                    </h3>
+                    <ul>
+                        {contributions.map((contribution, index) => (
+                            <li
+                                key={index}
+                                style={{
+                                    opacity: 0.6,
+                                    fontSize: "1rem",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: "4px",
+                                    marginTop: "0.5rem",
+                                    textTransform: "uppercase",
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        height: "3px",
+                                        width: "3px",
+                                        background: "black",
+                                    }}
+                                />
+                                {contribution}
+                            </li>
+                        ))}
+                    </ul>
+                    <div
+                        style={{
+                            fontSize: "1rem",
+                            display: "flex",
+                            alignItems: "flex-end",
+                            gap: "4px",
+                            fontWeight: "700",
+                            justifyContent: "space-between",
+                            width: "100%",
+                            marginTop: "1.6rem",
+                        }}
+                    >
+                        <div
+                            style={{
+                                opacity: 0.67,
+                                fontSize: "1rem",
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "4px",
+                                marginTop: "0.5rem",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    height: "3px",
+                                    width: "3px",
+                                    background: "black",
+                                    lineHeight: 0.6,
+                                }}
+                            />
+
+                            <div style={{}}>Year</div>
+                        </div>
+                        <div
+                            style={{
+                                opacity: 0.67,
+                                fontSize: "3rem",
+                                marginLeft: "0.6rem",
+                                lineHeight: 0.8,
+                            }}
+                        >
+                            {year}
+                        </div>
+                        <div style={{ flexGrow: 1 }} />
+                        <Link
+                            target="_blank"
+                            href={href}
+                            style={{
+                                display: "inline-block",
+                                fontSize: "1rem",
+                                color: "#FF1F55",
+                                border: "1px solid #FF1F55",
+                                padding: "0.5rem 1.6rem",
+                                textDecoration: "none",
+                                cursor: "pointer",
+                                fontFamily: "Aeonik",
+                                fontWeight: "300",
+                                fontSize: "1.2rem",
+                            }}
+                        >
+                            Visit Site
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,169 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Project from "./Components/Project";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const skills = [
+    "Website Consulting",
+    "UI/UX Design",
+    "Frontend Development",
+    "Backend Development",
+    "SEO",
+];
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    return (
+        <main
+            className="x-padding"
+            style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "flex-end",
+                    marginTop: "1rem",
+                }}
+            >
+                <svg height="3.6rem" viewBox="0 0 14 12" fill="none">
+                    <g clipPath="url(#clip0_67_213)">
+                        <rect width="14" height="12" fill="white" />
+                        <path
+                            d="M1 1V9C1 10.1046 1.89543 11 3 11V11C4.10457 11 5 10.1046 5 9V1"
+                            stroke="black"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                        <path
+                            d="M8.99902 1V11"
+                            stroke="black"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                        <rect
+                            x="12"
+                            y="9.99997"
+                            width="2"
+                            height="2"
+                            rx="1"
+                            fill="black"
+                        />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_67_213">
+                            <rect width="14" height="12" fill="white" />
+                        </clipPath>
+                    </defs>
+                </svg>
+                <div style={{
+                  fontSize: "1.6rem",
+                  fontWeight: 300,
+                  textTransform: "uppercase",
+                  lineHeight: 0.8,
+                  marginLeft: "1rem",
+                  fontStyle: "italic",
+
+                }}>Design Studio</div>
+            </div>
+            <div
+                className="about-container"
+                style={{
+                    fontFamily: "favorit Mono",
+                    display: "flex",
+                    width: "100%",
+                }}
+            >
+                <div
+                    className="about-image"
+                    style={{
+                        position: "relative",
+                        borderRadius: "100%",
+                    }}
+                >
+                    <Image
+                        fill
+                        src={"/head.png"}
+                        alt={"image"}
+                        sizes="30vw"
+                        priority
+                        style={{
+                            borderRadius: "100%",
+                            objectFit: "cover",
+                        }}
+                    />
+                </div>
+                <div
+                    className="about-text"
+                    style={{
+                        maxWidth: "30rem",
+                        opacity: 0.5,
+                    }}
+                >{`Phoenix Construction Resources is your go-to source for flexible, skilled labor on construction sites across the New England area. When your project requires additional manpower, we supply the dependable.`}</div>
+            </div>
+            <div
+                style={{
+                    fontFamily: "favorit Mono",
+                    textTransform: "uppercase",
+                    opacity: 0.5,
+                    marginTop: "6rem",
+                }}
+            >
+                // Projects
+            </div>
+            <Project
+                images={["/3law.png"]}
+                title={"3Laws Robotics"}
+                text={`Phoenix Construction Resources is your go-to source for flexible, skilled labor on construction sites across the New England area. When your project requires additional manpower, we supply the dependable.`}
+                contributions={[
+                    "UI/UX Design",
+                    "Frontend Development",
+                    "Backend Development",
+                ]}
+                year={2023}
+                href={"https://3laws.tech"}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <Project
+                images={["/larn.png"]}
+                title={"Larnelle Art"}
+                text={`Phoenix Construction Resources is your go-to source for flexible, skilled labor on construction sites across the New England area. When your project requires additional manpower, we supply the dependable.`}
+                contributions={[
+                    "UI/UX Design",
+                    "Frontend Development",
+                    "Backend Development",
+                ]}
+                year={2023}
+                href={"https://www.larnelleart.com"}
+            />
+            <Project
+                images={["/sibo.png"]}
+                title={"Sibo Chocolate"}
+                text={`Phoenix Construction Resources is your go-to source for flexible, skilled labor on construction sites across the New England area. When your project requires additional manpower, we supply the dependable.`}
+                contributions={[
+                    "UI/UX Design",
+                    "Frontend Development",
+                    "E-Commerce Integration",
+                ]}
+                year={2023}
+                href={"https://sibo-chocolate.vercel.app"}
+            />
+            <div style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: 'flex-end',
+              opacity: 0.5,
+              marginBottom: "2rem",
+              gap: '2rem',
+              whiteSpace: 'nowrap',
+            }}>
+              <div>emilio@ui-design-studio.com</div><div>781-879-7402</div>
+            </div>
+        </main>
+    );
 }
